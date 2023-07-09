1298d19407ce6fd268a15a5452b55d478d5039b9
PokeJofeJr4th <pokejofejr4th@gmail.com>
2023-07-09 19:26:52 -0400

    added a tiny test, moved parse xml into a separate fn might not compile because my thing is broken

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 /// This is the entrypoint for a program meant for development and testing of features.
 /// It may later become a CLI script for scholarwander, but feel free to use it and hack on it for development and testing
+#![warn(clippy::nursery, clippy::pedantic)]
 
 use clap::Parser;
-use std::env;
-use std::fs;
-use std::fmt;
 use openssl::cms;
+use std::env;
+use std::fmt;
+use std::fs;
 
 use std::io::{BufReader, Cursor};
 use xml::reader::{EventReader, XmlEvent};
+
+#[cfg(test)]
+mod tests;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -21,52 +25,51 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    
-    let cloudconfig_cipherdata: Vec<u8> = fs::read(args.cloudconfig)
-        .expect("Should have been able to read the file");
+    let cloudconfig_cipherdata: Vec<u8> =
+        fs::read(args.cloudconfig).expect("Should have been able to read the file");
 
     let config = cms::CMSOptions::NO_SIGNER_CERT_VERIFY;
     // config::set()
 
     if let Ok(mut cmsinfo) = cms::CmsContentInfo::from_der(&cloudconfig_cipherdata) {
-
-        let mut outData:Vec<u8> = vec![];
+        let mut outData: Vec<u8> = Vec::new();
         if let Ok(result) = cmsinfo.verify(
-            None, //certs: 
-            None, //store: 
-            None, //detached_data: 
-            Some(&mut outData), //output_data: 
-            config //flags: 
-        ){
-
+            None,               //certs:
+            None,               //store:
+            None,               //detached_data:
+            Some(&mut outData), //output_data:
+            config,             //flags:
+        ) {
             // if let Ok(res_str) = String::from_utf8(outData.clone()) {
             //     println!("{}", res_str);
             // }
 
             let data = BufReader::new(outData.as_slice()); // Buffering is important for performance
             let parser = EventReader::new(data);
-            let mut depth = 0;
-            for e in parser {
-                match e {
-                    Ok(XmlEvent::StartElement { name, .. }) => {
-                        println!("{:spaces$}+{name}", "", spaces = depth * 2);
-                        depth += 1;
-                    }
-                    Ok(XmlEvent::EndElement { name }) => {
-                        depth -= 1;
-                        println!("{:spaces$}-{name}", "", spaces = depth * 2);
-                    }
-                    Err(e) => {
-                        eprintln!("Error: {e}");
-                        break;
-                    }
-                    // There's more: https://docs.rs/xml-rs/latest/xml/reader/enum.XmlEvent.html
-                    _ => {}
-                }
-            }
-
+            parse_xml(parser)?
         }
-
     }
-    
+}
+
+fn parse_xml(xml: EventReader) -> Result<i32, String> {
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                println!("{:spaces$}+{name}", "", spaces = depth * 2);
+                depth += 1;
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                depth -= 1;
+                println!("{:spaces$}-{name}", "", spaces = depth * 2);
+            }
+            Err(e) => {
+                eprintln!("Error: {e}");
+                break;
+            }
+            // There's more: https://docs.rs/xml-rs/latest/xml/reader/enum.XmlEvent.html
+            _ => {}
+        }
+    }
+    Ok(depth)
 }
---
+++ src/tests.rs
@@ -0,0 +1,7 @@
+use std::io::BufReader;
+
+#[test]
+fn parsing() {
+    let bufreader = BufReader::new("<paladinResponse xmlns=\"http://schemas.securew2.com/paladinResponse\" type=\"7\" version=\"1.2.1\">
+");
+}

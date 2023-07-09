22115577020c8b8e9ba2373b01f513e9c272e234
PokeJofeJr4th <pokejofejr4th@gmail.com>
2023-07-09 19:27:49 -0400

    .

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,10 @@
 use std::io::BufReader;
 
+use crate::parse_xml;
+
 #[test]
 fn parsing() {
     let bufreader = BufReader::new("<paladinResponse xmlns=\"http://schemas.securew2.com/paladinResponse\" type=\"7\" version=\"1.2.1\">
 ");
+    parse_xml(bufreader)?;
 }
